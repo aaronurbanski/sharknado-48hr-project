9d0a1683af3f5cadcff7c2e5da295ec33e589de2
Jordan <jshea.social@gmail.com>
2020-11-02 17:37:39 -0500

    celsius shortner

--- src/components/OpenWeather.js
+++ src/components/OpenWeather.js
@@ -30,10 +30,13 @@ const OpenWeather = (props) => {
     }
     }, [latitude, longitude]);
 
+    let far = results.main.temp;
+    let cel = (results.main.temp - 32) * 5/9;
+
 
       return(
         <div>
-          <p>{units === "F" ? results.main.temp : (results.main.temp - 32) * 5/9} {units}</p>
+          <p>{units === "F" ? far : cel.toFixed(2)} {units}</p>
       <p><img src={`https://openweathermap.org/img/wn/${results.weather[0].icon}.png`} alt="icon" /><br/><span>{results.weather[0].description}</span></p>
           <Switch
             checked={units === "F" ? true : false}
